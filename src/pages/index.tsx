032408c0066d5a6ccccabd2df88ab5fb42a3535d
Otto Laitinen <otto.laitinen@hotmail.fi>
2021-10-29 19:26:03 +0200

    feat: installed styled components and changed some files to tc

---
+++ src/pages/index.tsx
@@ -0,0 +1,64 @@
+import * as React from "react"
+import { graphql, PageProps, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+
+const IndexPage: React.FC<IndexPageProps> = props => {
+  const { data } = props
+  const profilePicture = getImage(
+    data.strapiHome.profilePicture.localFile.childImageSharp.gatsbyImageData
+  )
+  console.log(props.path)
+  return (
+    <Layout>
+      <h1>{data.strapiHome.title}</h1>
+      <p>{data.strapiHome.bio}</p>
+      {profilePicture && (
+        <GatsbyImage image={profilePicture} alt={"Pic of me lol"} />
+      )}
+    </Layout>
+  )
+}
+
+interface IndexPageProps extends PageProps {
+  data: {
+    strapiHome: {
+      title: string
+      bio: string
+      profilePicture: {
+        localFile: {
+          childImageSharp: {
+            gatsbyImageData: ImageDataLike
+          }
+        }
+      }
+      seo: {
+        title: string
+        description: string
+      }
+    }
+  }
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    strapiHome {
+      title
+      bio
+      profilePicture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(width: 660)
+          }
+        }
+      }
+      seo {
+        title
+        description
+      }
+    }
+  }
+`
+
+export default IndexPage
